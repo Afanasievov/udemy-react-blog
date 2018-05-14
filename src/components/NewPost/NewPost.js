2456b026343e4e46709074da0f0bdf525a34c33a
Oleksandr Afanasiev <afanasievov@gmail.com>
2018-05-15 00:03:19 +0300

    Fetch a post from API and update the FullPost component

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -20,7 +20,7 @@ class NewPost extends Component {
         <label>Author</label>
         <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
           <option value="Max">Max</option>
-          <option value="Oleksandr">Manu</option>
+          <option value="Oleksandr">Oleksandr</option>
         </select>
         <button>Add Post</button>
       </div>
