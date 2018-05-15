d3c29a0268ee48005b603f6cce2b2ee76307e79a
Oleksandr Afanasiev <afanasievov@gmail.com>
2018-05-15 08:01:23 +0300

    Add axios interceptors.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use((request) => {
+  console.log('axios request:', request);
+  return request;
+}, (error) => {
+  console.log('axios error:', error);
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use((response) => {
+  console.log('axios response:', response);
+  return response;
+}, (error) => {
+  console.log('axios error:', error);
+  return Promise.reject(error);
+})
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
