99633748625c604cd02fecba984f937043ffef2e
Oleksandr Afanasiev <afanasievov@gmail.com>
2018-05-15 07:40:54 +0300

    Add Post and Delete requests.

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -7,6 +7,7 @@ class FullPost extends Component {
   state = {
     loadedPost: null
   }
+
   componentDidUpdate(prevProps, prevState) {
     if (this.props.id) {
       if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
@@ -17,6 +18,14 @@ class FullPost extends Component {
       }
     }
   }
+
+  deletePostHandler = () => {
+    axios.delete(`http://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+      .then((response) => {
+        console.log('Delete response:', response);
+      })
+  }
+
   render () {
     let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
     if (this.props.id) {
@@ -28,7 +37,7 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
-            <button className="Delete">Delete</button>
+            <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
           </div>
         </div>
       );
